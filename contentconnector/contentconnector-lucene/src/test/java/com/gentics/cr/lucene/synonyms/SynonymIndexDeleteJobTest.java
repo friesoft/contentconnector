6b80b61a638c41d9db2563c9b583bab230e77352
Andreas Perhab <bigbear.ap@gmail.com>
2013-11-12 12:57:13 +0100

    added timeout in  setup function of test to get a response in case another test forgot to close an index location properly

--- contentconnector/contentconnector-lucene/src/test/java/com/gentics/cr/lucene/synonyms/SynonymIndexDeleteJobTest.java
+++ contentconnector/contentconnector-lucene/src/test/java/com/gentics/cr/lucene/synonyms/SynonymIndexDeleteJobTest.java
@@ -1,9 +1,18 @@
 package com.gentics.cr.lucene.synonyms;
 
+import static junit.framework.Assert.assertTrue;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -40,31 +49,39 @@ public class SynonymIndexDeleteJobTest {
 	}
 
 	@Before
-	public void create() throws CRException, FileNotFoundException, URISyntaxException {
-		GenericConfiguration genericConf = new GenericConfiguration();
-		try {
-			String confPath2 = new File(this.getClass().getResource("indexer.properties").toURI()).getParentFile().getAbsolutePath();
-			GenericConfigurationFileLoader.load(genericConf, confPath2 + "/indexer.properties");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		CRConfigUtil config = new CRConfigUtil(genericConf, "DEFAULT");
-
-		GenericConfiguration sc = new GenericConfiguration();
-		sc.set("indexLocations.1.path", "RAM_1");
-		sc.set("indexLocationClass", "com.gentics.cr.lucene.indexer.index.LuceneSingleIndexLocation");
-
-		CRConfig singleConfig1 = new CRConfigUtil(sc, "sc1");
-		singleLoc1 = LuceneIndexLocation.getIndexLocation(singleConfig1);
-
-		config2 = new CRConfigUtil(config.getSubConfig("index").getSubConfig("DEFAULT").getSubConfig("extensions").getSubConfig("SYN"),
-				"SYN");
-
-		indexExtension = new SynonymIndexExtension(config2, singleLoc1);
-
-		SynonymIndexJob job = new SynonymIndexJob(config2, singleLoc1, indexExtension);
-		job.run();
-
+	public void create() throws CRException, FileNotFoundException, URISyntaxException, InterruptedException, ExecutionException, TimeoutException {
+		ExecutorService exec = Executors.newSingleThreadExecutor();
+		final Callable<Boolean> task = new Callable<Boolean>() {
+			@Override
+			public Boolean call() throws Exception {
+				GenericConfiguration genericConf = new GenericConfiguration();
+				try {
+					String confPath2 = new File(this.getClass().getResource("indexer.properties").toURI()).getParentFile().getAbsolutePath();
+					GenericConfigurationFileLoader.load(genericConf, confPath2 + "/indexer.properties");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				CRConfigUtil config = new CRConfigUtil(genericConf, "DEFAULT");
+		
+				GenericConfiguration sc = new GenericConfiguration();
+				sc.set("indexLocations.1.path", "RAM_1");
+				sc.set("indexLocationClass", "com.gentics.cr.lucene.indexer.index.LuceneSingleIndexLocation");
+		
+				CRConfig singleConfig1 = new CRConfigUtil(sc, "sc1");
+				singleLoc1 = LuceneIndexLocation.getIndexLocation(singleConfig1);
+		
+				config2 = new CRConfigUtil(config.getSubConfig("index").getSubConfig("DEFAULT").getSubConfig("extensions").getSubConfig("SYN"),
+						"SYN");
+		
+				indexExtension = new SynonymIndexExtension(config2, singleLoc1);
+		
+				SynonymIndexJob job = new SynonymIndexJob(config2, singleLoc1, indexExtension);
+				job.run();
+				return true;
+			}
+		};
+		final Future<Boolean> future = exec.submit(task);
+		assertTrue(future.get(10000, TimeUnit.MILLISECONDS));
 	}
 
 	@Test
